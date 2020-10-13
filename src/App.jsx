d75e12a3a7813c2a346249337fad467a8a394e13
yamaguchiosamu <66462182+yamaguchiosamu@users.noreply.github.com>
2020-10-13 15:46:57 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import defaultDataset from "./dataset";
+import './assets/styles/style.css'
+
+export default class App extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            answer: [],
+            chats: [],
+            currentID: "init",
+            dataset: defaultDataset,
+            open: false
+        }
+    }
+  
+    render() {
+    return (
+      <section className="c-section">
+          <div className="c-box">
+              {this.state.currentID}
+          </div>
+      </section>
+    );
+    }
+  }
+
+
