52d6f4ffcf2424c36b42cebcec86ffe43f289e76
yamaguchiosamu <66462182+yamaguchiosamu@users.noreply.github.com>
2020-10-15 11:15:05 +0900

    #6-9min

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import defaultDataset from "./dataset";
 import './assets/styles/style.css'
-import {AnswersList} from "./components/index";
+import {AnswersList, Chats} from "./components/index";
 
 export default class App extends React.Component {
     constructor (props) {
@@ -33,6 +33,7 @@ export default class App extends React.Component {
         return (
             <section className="c-section">
                 <div className="c-box">
+                    <Chats />
                     <AnswersList answers={this.state.answers} />
                 </div>
             </section>
