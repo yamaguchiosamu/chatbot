a32648e76b08054da8707e099f68825ad94a0b7e
yamaguchiosamu <66462182+yamaguchiosamu@users.noreply.github.com>
2020-10-15 13:30:38 +0900

    #6-12min

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import Typography from '@material-ui/core/Typography';
 
 const Chat = () => {
-
+    return (
+        <ListItem>
+            <ListItemAvatar>
+                <Avatar alt="icon" src="/static/images/avatar/1.jpg" />
+            </ListItemAvatar>
+            <div className="p-chat__bubble">ダミーテキスト</div>
+            </ListItem>
+    )
 }
 
 export default Chat
