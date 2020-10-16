14868adde21b91fadd70b81a099178bd73c38661
yamaguchiosamu <66462182+yamaguchiosamu@users.noreply.github.com>
2020-10-16 23:59:35 +0900

    #9fin

--- src/components/Forms/FormDialog.jsx
+++ src/components/Forms/FormDialog.jsx
@@ -3,17 +3,63 @@ import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import TextInput from './TextInput'
 
 
 export default class FormDialog extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            name: "",
+            email: "",
+            description: ""
+        }
+
+        this.imputName = this.imputName.bind(this)
+        this.imputEmail = this.imputEmail.bind(this)
+        this.imputDescription = this.imputDescription.bind(this)
     }
 
+    imputName = (event) => {
+        this.setState({ name: event.target.value })
+    }
 
+    imputEmail = (event) => {
+        this.setState({ email: event.target.value })
+    }
 
+    imputDescription = (event) => {
+        this.setState({ description: event.target.value })
+    }
+
+    submitForm = () => {
+        const name = this.state.name
+        const email = this.state.email
+        const description = this.state.description
+
+        const payload = {
+            text:'お問い合わせがありました\n' +
+                    'お名前：' + name + '\n' +
+                    'Email：' + email + '\n' +
+                    '問い合せ内容：\n' + description 
+        }
+
+        const url = 'https://hooks.slack.com/services/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'
+
+        fetch(url,{
+            method: 'POST',
+            body: JSON.stringify(payload)
+        }).then(() =>{
+            alert('送信が完了しました。。追ってご連絡します！')
+            this.setState({
+                name: "",
+                email: "",
+                description:""
+            })
+            return this.props.handleClose()
+        })
+    }
 
     render() {
         return(
@@ -23,19 +69,27 @@ export default class FormDialog extends React.Component {
                 aria-labelledby="alert-dialog-title"
                 aria-describedby="alert-dialog-description"
                 >
-                <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+                <DialogTitle id="alert-dialog-title">お問い合わせフォーム</DialogTitle>
                 <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        Let Google help apps determine location. This means sending anonymous location data to
-                        Google, even when no apps are running.
-                </DialogContentText>
+                    <TextInput 
+                        label={"お名前(必須)"} multiline={false} rows={1}
+                        value={this.state.name} type={"text"} onChange={this.imputName}
+                    />
+                    <TextInput 
+                        label={"メールアドレス(必須)"} multiline={false} rows={1}
+                        value={this.state.email} type={"email"} onChange={this.imputEmail}
+                    />
+                    <TextInput 
+                        label={"お問い合わせ内容(必須)"} multiline={true} rows={5}
+                        value={this.state.description} type={"text"} onChange={this.imputDescription}
+                    />
                 </DialogContent>
             <DialogActions>
                 <Button onClick={this.props.handleClose} color="primary">
-                    Disagree
+                    キャンセル
                 </Button>
-                <Button onClick={this.props.handleClose} color="primary" autoFocus>
-                    Agree
+                <Button onClick={this.submitForm} color="primary" autoFocus>
+                    送信する
                 </Button>
             </DialogActions>
             </Dialog>
